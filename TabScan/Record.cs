64b791efbf3021ed6c11a6c97646ceb537455595
GabrielaSolak <gabrielasolak00@gmail.com>
2026-09-24 13:24:41 +0200

    Some progress

--- TabScan/Record.cs
+++ TabScan/Record.cs
@@ -13,24 +13,46 @@ public class Record
         }
     }
 
-    public List<int> StudentIDs { get; set; }
+    public Student? StudentID { get; set; }
     public string TabletId { get; set; }
     public DateTime StartTime { get; set; }
     public DateTime EndTime { get; set; }
+    public string TimeLeft
+    {
+        get
+        {
+            TimeSpan ts = EndTime - GetNow();
+            string res = "";
+            if (ts.TotalDays >= 1)
+            {
+                return $"{(int)ts.TotalDays}d {(int)ts.TotalHours % 24}h";
+            }
+            if(ts.TotalHours >= 1)
+            {
+                return $"{(int)ts.TotalHours}h {(int)ts.TotalMinutes % 60}min";
+            }
+
+            return $"{(int)ts.TotalMinutes}min {(int)ts.TotalSeconds % 60}s";
+        }
+    }
+
+    public static DateTime GetNow()
+    {
+        return DateTime.Now;
+    }
 
     public Record()
     {
         id = ID++;
-        StudentIDs = new List<int>();
         TabletId = string.Empty;
         StartTime = DateTime.Now;
         EndTime = DateTime.Now;
     }
 
-    public Record(List<int> studentIDs, string tabletId, DateTime startTime, DateTime endTime)
+    public Record(Student? studentID, string tabletId, DateTime startTime, DateTime endTime)
     {
         id = ID++;
-        StudentIDs = studentIDs;
+        StudentID = studentID;
         TabletId = tabletId;
         if(endTime < startTime) throw new Exception("endTime must be greater than startTime");
         StartTime = startTime;
